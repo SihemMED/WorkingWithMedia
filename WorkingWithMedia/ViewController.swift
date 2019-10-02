09a1b2dcaa0ce5871815c36c31da9d6c1af63b34
sihem.mohamed <sihem.mohamed@sofrecom.com>
2019-10-02 13:52:01 +0100

    Initial Commit

---
+++ WorkingWithMedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkingWithMedia
+//
+//  Created by Sofrecom2 on 10/2/19.
+//  Copyright © 2019 Simo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WorkingWithMediaTests/WorkingWithMediaTests.swift
@@ -0,0 +1,34 @@
+//
+//  WorkingWithMediaTests.swift
+//  WorkingWithMediaTests
+//
+//  Created by Sofrecom2 on 10/2/19.
+//  Copyright © 2019 Simo. All rights reserved.
+//
+
+import XCTest
+@testable import WorkingWithMedia
+
+class WorkingWithMediaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
